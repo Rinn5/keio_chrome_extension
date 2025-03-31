cd7849a68f317f2ce76270fedb487dcc361570e2
okahararintaro <rintaro.okahara@rooter.co.jp>
2025-04-01 07:01:50 +0900

    構築

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,12 @@
 chrome.runtime.onInstalled.addListener((details) => {
+  chrome.action.setIcon({
+    path: {
+      19: 'images/icon-16.png',
+      38: 'images/icon-48.png'
+    }
+  });
   if (details.reason ===  "install"){
-    chrome.tabs.create({url: "first_setting.html"})
+    chrome.tabs.create({url: "first_setting.html"});
   } else if (details.reason === "update"){
     
   }
