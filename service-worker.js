5d0b912b6980d3743ca6ff7537f6eeb7869bb7d1
okahararintaro <rintaro.okahara@rooter.co.jp>
2025-04-01 07:54:32 +0900

    写真名変更、iconに19px, 38pxを追加

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,8 @@
 chrome.runtime.onInstalled.addListener((details) => {
   chrome.action.setIcon({
     path: {
-      16: 'images/icon16.png',
-      48: 'images/icon48.png'
+      16: 'images/icon19.png',
+      48: 'images/icon38.png'
     }
   });
   if (details.reason ===  "install"){
