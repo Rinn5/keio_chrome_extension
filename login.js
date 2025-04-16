b1581c85ac21407753f430a79700e643b1314d3f
okahararintaro <rintaro.okahara@rooter.co.jp>
2025-04-17 05:41:45 +0900

    更新

--- login.js
+++ login.js
@@ -1,4 +1,3 @@
-/* 慶應ログイン画面での実際の自動ログイン処理 */
 chrome.storage.local.get(["username", "password"], function(data) {
   const startInput = () => {
     const first_input = document.querySelector("input[name='identifier']");
@@ -12,7 +11,7 @@ chrome.storage.local.get(["username", "password"], function(data) {
     } else {
       setTimeout(startInput, 300);
     }
-  }
+  };
 
   const waitForPassword = () => {
     const second_input = document.querySelector("input[type='password']");
@@ -21,11 +20,38 @@ chrome.storage.local.get(["username", "password"], function(data) {
       second_input.dispatchEvent(new Event("input", { bubbles: true }));
       setTimeout(() => {
         document.querySelector("input[value='確認']").click();
+        setTimeout(handleNextScreen, 300);
       }, 300);
     } else {
       setTimeout(waitForPassword, 3000);
     }
-  }
-  
-  startInput();
-})
+  };
+
+  const handleNextScreen = (onFail) => {
+    const maxAttempts = 5;
+    let attempts = 0;
+
+    const check = () => {
+      const current_url = window.location.href;
+      const isTargetURL = current_url.includes("https://auth.keio.jp/idp/profile/");
+
+      if (isTargetURL) {
+        const btn = document.querySelector("div.confirm_exp li a");
+        if (btn) {
+          btn.click();
+          return;
+        }
+      }
+
+      attempts++;
+      if (attempts < maxAttempts) {
+        setTimeout(check, 300);
+      } else {
+        if (onFail) onFail();
+      }
+    };
+
+    check();
+  };
+  handleNextScreen(startInput);
+});
